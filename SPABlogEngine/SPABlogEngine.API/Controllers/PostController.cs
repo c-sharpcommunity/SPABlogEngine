2fffbb23fa8732ccc1ef2909802d40fc86d4178e
Khoa Truong Quoc Anh <khoa.truongquocanh@nashtechglobal.com>
2018-06-27 10:44:47 +0700

    add Authorize.

--- SPABlogEngine/SPABlogEngine.API/Controllers/PostController.cs
+++ SPABlogEngine/SPABlogEngine.API/Controllers/PostController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +9,7 @@ using SPABlogEngine.API.Models.Blog;
 
 namespace SPABlogEngine.API.Controllers
 {
+    [Authorize]
     public class PostController : Controller
     {
         private readonly ApplicationDbContext _context;
