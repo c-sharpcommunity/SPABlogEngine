3997d5d50b09a89d7a1351c3d985e111ae9aa4ec
Khoa Truong Quoc Anh <khoa.truongquocanh@nashtechglobal.com>
2018-06-27 09:38:38 +0700

    create migration.

--- SPABlogEngine/SPABlogEngine.API/Data/Migrations/20180627023629_SimpleBlogEngine.Designer.cs
+++ SPABlogEngine/SPABlogEngine.API/Data/Migrations/20180627023629_SimpleBlogEngine.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace SPABlogEngine.API.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180627023629_SimpleBlogEngine")]
+    partial class SimpleBlogEngine
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
